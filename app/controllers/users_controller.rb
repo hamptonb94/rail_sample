efd8c6afc68fb916af693c5d4cccffc940f2abe8
Bryan Hampton <bryan@zeropointfrontiers.com>
2013-07-04 23:22:45 -0500

    Adding authorization to edit and updates.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -20,11 +22,11 @@ class UsersController < ApplicationController
   end
   
   def edit
-  	@user = User.find(params[:id])
+  	# we set @user in correct_user()
   end
   
   def update
-    @user = User.find(params[:id])
+    # we set @user in correct_user()
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
       sign_in @user
@@ -33,4 +35,19 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  
+  
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
